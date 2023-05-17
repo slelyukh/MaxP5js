5925d74393a438cd01aa2b0bd59655ed19c4a7a6
Max713Cheung <max713cheung@gmail.com>
2023-05-17 21:43:13 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,39 @@
 function setup(){
   createCanvas(400, 400);
   background(200);
+  frameRate(1);
 }
-// test2!
 
-// test
+// TODO: make a drawMountains function(x, y, scale)
+// do everything like you did for player
+// use 3 triangles to make mountains
+
+// TODO: add a scale argument 
+function drawPlayer(x, y, scale) {
+  // hint: you should use scale to determine all of these variables
+  let bodyHeight = 75
+  let bodyWidth = 50
+  let armWidth = 50
+  let armHeight = 25
+  let legWidth = 25
+  let legHeight = 50
+  let headWidth = 50
+
+  // draw player body
+  rect(x-(bodyWidth/2), y-(bodyHeight/2), bodyWidth, bodyHeight);
+  // draw right arm
+  rect(x+(bodyWidth/2), y-(bodyHeight/2), armWidth, armHeight);
+  // draw left arm
+  rect(x-(bodyWidth/2)-armWidth, y-(bodyHeight/2), armWidth, armHeight);
+  // draw left leg
+  rect(x-(bodyWidth/2), y+(bodyHeight/2), legWidth, legHeight)
+  // draw right leg
+  rect(x, y+(bodyHeight/2), legWidth, legHeight)
+  //draw player head
+  circle(x, y-(bodyHeight/2)-(headWidth/2), headWidth)
+}
+
 function draw() {
+  background(Math.ceil(Math.random() * 256));
+  drawPlayer(200,200)
 }
